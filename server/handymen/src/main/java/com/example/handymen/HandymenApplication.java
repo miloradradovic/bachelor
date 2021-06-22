6bcb03a57d1dad25c1b0413bc704b89e6ba70a23
miloradradovic <miloradradovicmico98@gmail.com>
2021-06-22 21:06:14 +0200

    server change

---
+++ server/handymen/src/main/java/com/example/handymen/HandymenApplication.java
@@ -0,0 +1,13 @@
+package com.example.handymen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandymenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandymenApplication.class, args);
+	}
+
+}
